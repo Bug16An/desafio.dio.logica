587c71d43a2d7b9a00eb82d0d86ed40c485371b3
Bug16An <junior.h.ferrante@gmail.com>
2024-01-07 03:17:37 -0300

    commit correto 06/01/24

--- desafio.js
+++ desafio.js
@@ -1,41 +1,52 @@
 let nomeHeroi = "DesbugandoAprogramação"
-let XpDoHeroi = 9900
+let XpDoHeroi = 9000
 
-    switch (XpDoHeroi) {
-        case XpDoHeroi <= 1000:
-            console.log("Olá meu nome é " + nomeHeroi + " esta no nivel de Ferro")
-            break
-        case XpDoHeroi === 1001:
-            case XpDoHeroi <= 2000:
-            console.log("Olá meu nome é " + nomeHeroi + " esta no nivel de Bronze")
-            break
-        case XpDoHeroi === 2001:
-            case XpDoHeroi  <= 5000:
-            console.log("Olá meu nome é " + nomeHeroi + " esta no nivel de Prata")
-            break
-        case XpDoHeroi === 5001:
-            case XpDoHeroi <= 7000:
-            console.log("Olá meu nome é " + nomeHeroi + " esta no nivel de Ouro")
-            break
-        case XpDoHeroi === 7001:
-            case XpDoHeroi <= 8000:
-            console.log("Olá meu nome é " + nomeHeroi + " esta no nivel de Platina")
-            break
-            
-        case XpDoHeroi === 8001:
-            case XpDoHeroi <= 9000:
-            console.log("Olá meu nome é " + nomeHeroi + " esta no nivel de Ascendente")
-            break
-        case XpDoHeroi === 9001:
-            case XpDoHeroi <= 10000:
-            console.log("Olá meu nome é " + nomeHeroi + " esta no nivel de Imortal ")
-            break
-        case XpDoHeroi >=10001:
-            console.log("Olá meu nome é " + nomeHeroi + " esta no nivel de Radiante")
-            break }
+for (let Ferro = 0; Ferro <=1000; Ferro++){
+    if(XpDoHeroi === Ferro){
+        console.log("O Heroi" + nomeHeroi + " esta no nivel de Ferro")}
+    }
     
+for (let Bronze = 1001; Bronze <=2000; Bronze++){
+    if(XpDoHeroi === Bronze){
+        console.log("O heroi" + nomeHeroi + " esta no nivel de Bronze")}
+    }
+    
+for (let Prata = 2001; Prata <=5000; Prata++){
+    if(XpDoHeroi === Prata){
+        console.log("O Heroi " + nomeHeroi + " esta no nivel de Prata")}
+    }
 
-   
+for (let Ouro = 5001; Ouro <=7000; Ouro++){
+    if(XpDoHeroi === Ouro) {
+        console.log("O Heroi " + nomeHeroi + " esta no nivel de Ouro")}
+    }
+
+for (let Platina = 7001; Platina <= 8000; Platina++){
+    if(XpDoHeroi === Platina){
+        console.log("O Heroi " + nomeHeroi + " esta no nivel de Platina")}
+    }
+
+for (let Ascendente = 8001; Ascendente <= 9000; Ascendente++){
+    if(XpDoHeroi === Ascendente){
+        console.log("O Heroi " + nomeHeroi + " esta no nivel de Ascendente")}
+}
+
+for (let Imortal = 9001; Imortal<=10000; Imortal++){
+    if(XpDoHeroi === Imortal){
+        console.log("O Heroi " + nomeHeroi + " esta no nivel de Imortal")}
+    }
+
+for (let Radiante = 10001; Radiante =>10001; Radiante++){
+    if(XpDoHeroi === Radiante){
+        console.log("O Heroi " + nomeHeroi + " esta no nivel de Radiante")}
+    }
+
+ 
+
+
+
+
+//tem que usar um for 
             
 //usar node desafio.js para o terminal ler meu codigo 
 //para atualiza no github usar git add . depois git commit -m "algum nome" e por fim git push -u origin main 
